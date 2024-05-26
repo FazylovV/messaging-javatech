d66e843df9662294e895a6b4b5444889cb605aac
FazylovV <vladislav.fazylov04@gmail.com>
2024-05-27 02:07:58 +0500

    done

--- src/main/java/com/example/messagingrabbitmq/MessagingRabbitmqApplication.java
+++ src/main/java/com/example/messagingrabbitmq/MessagingRabbitmqApplication.java
@@ -1,9 +1,6 @@
 package com.example.messagingrabbitmq;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
@@ -11,40 +8,41 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class MessagingRabbitmqApplication {
 
-	static final String topicExchangeName = "spring-boot-exchange";
+	//static final String fanoutExchangeName = "spring-boot-exchange";
 
-	static final String queueName = "spring-boot";
+	private String queueName = "spring-boot" + UUID.randomUUID();
 
 	@Bean
-	Queue queue() {
-		return new Queue(queueName, false);
+	protected Queue queue() {
+		return new Queue(queueName, false, true, true);
 	}
 
 	@Bean
-	TopicExchange exchange() {
-		return new TopicExchange(topicExchangeName);
+	protected FanoutExchange exchange() {
+		return new FanoutExchange("spring-boot-exchange");
 	}
 
 	@Bean
-	Binding binding(Queue queue, TopicExchange exchange) {
-		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+	protected Binding binding(Queue queue, FanoutExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange);
 	}
 
 	@Bean
-	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-			MessageListenerAdapter listenerAdapter) {
-		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory);
+	protected SimpleMessageListenerContainer container(ConnectionFactory factory,
+			MessageListenerAdapter adapter) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(factory);
 		container.setQueueNames(queueName);
-		container.setMessageListener(listenerAdapter);
+		container.setMessageListener(adapter);
 		return container;
 	}
 
 	@Bean
-	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+	protected MessageListenerAdapter listenerAdapter(Receiver receiver) {
 		return new MessageListenerAdapter(receiver, "receiveMessage");
 	}
 
--- src/main/java/com/example/messagingrabbitmq/Runner.java
+++ src/main/java/com/example/messagingrabbitmq/Runner.java
@@ -1,27 +1,30 @@
 package com.example.messagingrabbitmq;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Runner implements CommandLineRunner {
 
-	private final RabbitTemplate rabbitTemplate;
-	private final Receiver receiver;
-
-	public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
-		this.receiver = receiver;
-		this.rabbitTemplate = rabbitTemplate;
-	}
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	@Autowired
+	private Receiver receiver;
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Sending message...");
-		rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
-		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		Scanner reader = new Scanner(System.in);
+		while (true) {
+			System.out.println("Send message:");
+			String message = reader.nextLine();
+			rabbitTemplate.convertAndSend("spring-boot-exchange", "foo.bar.baz", message);
+			receiver.latch.await(10000, TimeUnit.MILLISECONDS);
+		}
 	}
 
 }
